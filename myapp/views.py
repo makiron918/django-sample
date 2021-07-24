5b1a695cb3bbe99491542145cc30ec53f65ba225
makiron918 <gonzaress918@gmail.com>
2021-07-24 10:48:02 +0900

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def index(request):
   return render(request,'index.html')
+
+def welcome(request): # welcome関数
+	return render(request, 'welcome.html') # welcome.htmlを返す
+
+def like(request): # like関数
+  return render(request,'like.html') # like.htmlを返す
+
+def dislike(request): # dislike関数
+  return render(request,'dislike.html') # dislike.htmlを返す
+
+def birthday(request): # birthday関数
+  return render(request,'birthday.html') # birthday.htmlを返す
